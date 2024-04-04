007d09976225ad6c1ea007871404b4f46e2f2f39
Daz DeBoer <daz@gradle.com>
2024-04-04 11:12:32 -0600

    Constrain okio in classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        constraints {
+            classpath("com.squareup.okio:okio:3.4.0")
+        }
+    }
+}
 plugins {
     alias(libs.plugins.versions)
 }
